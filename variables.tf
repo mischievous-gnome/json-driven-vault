07500f296f1bfb7a0f6ad64463fd3c62bcfbc1e4
Ryan Casey <ryan.casey@hashicorp.com>
2024-06-20 09:51:42 -0400

    Fixed typo

--- variables.tf
+++ variables.tf
@@ -4,22 +4,22 @@ variable "ldap_secrets_data" {
 }
 
 variable "kvv2_secrets_data" {
-  description = "Localtion of KVV2 files for KVV2 secrets mounts"
+  description = "Location of KVV2 files for KVV2 secrets mounts"
   default     = "data/secrets/kvv2"
 }
 
 variable "k8s_secrets_data" {
-  description = "Localtion of Kubernetes files for Kubernetes secrets mounts"
+  description = "Location of Kubernetes files for Kubernetes secrets mounts"
   default     = "data/secrets/k8s"
 }
 
 variable "generic_secrets_data" {
-  description = "Localtion of files for generic Vault secrets mounts"
+  description = "Location of files for generic Vault secrets mounts"
   default     = "data/secrets/generic_vault_mount"
 }
 
 variable "consul_secrets_data" {
-  description = "Localtion of files for generic Vault secrets mounts"
+  description = "Location of files for generic Vault secrets mounts"
   default     = "data/secrets/consul"
 }
 
